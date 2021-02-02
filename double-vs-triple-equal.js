3bf1b046079614846739b4492902d40466d297be
hasansami75 <mehedihasan3175@gmail.com>
2021-02-03 00:13:41 +0600

    double-vs-triple-equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+//double equal e same type hole true hoye jay , jemon 0 & false , 1 & true k double equal true dhorse, kintu triple equal false dhorbe
+
+const first = 0;
+const second = false;
+if(first === second){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
