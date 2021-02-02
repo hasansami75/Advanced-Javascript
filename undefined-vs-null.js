592f324ac3382f279915ac7716ec71bd7b3a01b0
hasansami75 <mehedihasan3175@gmail.com>
2021-02-03 00:04:25 +0600

    null-vs-undefined cleared

---
+++ undefined-vs-null.js
@@ -0,0 +1,21 @@
+//undefined
+variable er value declare na korle undefined hobe
+let pakhi;
+function er parameter declare kore value return na korle undefined
+function add (num1, num2){
+   console.log(num1 + num2);
+   return}
+
+function er parameter declare na korle auto undefined hoy
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+object access korar somoy jodi object er parameter na thakle undefined
+const premik = {name: 'smart dude', phone: 567453} ;
+console.log(premik.gf);
+let fun = undefined;
+console.log(fun);
+
+//null
+kunu jinihs khali thakle seta hoye jay null
