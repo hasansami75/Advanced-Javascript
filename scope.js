d063ca5c7fd82edbf678e23677dad55c6cf27dbc
hasansami75 <mehedihasan3175@gmail.com>
2021-02-03 15:24:18 +0600

    scope,block scope

---
+++ scope.js
@@ -0,0 +1,12 @@
+let bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    return result;
+}
+
+
+
+var day = 'friday';
+console.log(day);
+const output = sum(3, 7);
+console.log(output);
