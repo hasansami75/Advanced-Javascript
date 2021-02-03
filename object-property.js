662a0d653160b8a941660a1fa5f5d4808f5e261c
hasansami75 <mehedihasan3175@gmail.com>
2021-02-03 15:06:07 +0600

    advance map-filter

---
+++ object-property.js
@@ -0,0 +1,12 @@
+const students = [
+    {id: 21, name: 'Omar Sunny'},
+    {id: 31, name: 'Maannnaaa'},
+    {id: 41, name: 'Moyouri'},
+    {id: 71, name: 'Dipzol'}
+]
+
+const names = students.map(s => s.name);
+const ids = students.map(s => s.id);
+const bigger = students.filter(s=> s.id>40);
+const bigger1 = students.find(s=> s.id>40);
+console.log(bigger1);
